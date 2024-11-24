71e5f3d9e3ab174ff39cc45ed31ec110716419d9
Alexander Stekelenburg <anoniem.axander@gmail.com>
2024-11-24 14:39:25 +0100

    Small optimisations

--- src/day21.rs
+++ src/day21.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
 
+use crate::common;
+use bit_vec::BitVec;
+use itertools::repeat_n;
 use std::collections::HashMap;
 
-use crate::common;
-
-type Grid = Vec<Vec<bool>>;
+type Grid = Vec<BitVec>;
 
 fn small_grids(grid: &Grid, size: usize) -> Vec<Grid> {
     let mut small_grids = Vec::new();
@@ -12,11 +13,8 @@ fn small_grids(grid: &Grid, size: usize) -> Vec<Grid> {
         for y in 0..(grid.len() / size) {
             for x in 0..(grid.len() / size) {
                 small_grids.push(vec![
-                    vec![grid[y * size][x * size], grid[y * size][x * size + 1]],
-                    vec![
-                        grid[y * size + 1][x * size],
-                        grid[y * size + 1][x * size + 1],
-                    ],
+                    BitVec::from_fn(2, |i| grid[y * size][x * size + i]),
+                    BitVec::from_fn(2, |i| grid[y * size + 1][x * size + i]),
                 ]);
             }
         }
@@ -24,21 +22,9 @@ fn small_grids(grid: &Grid, size: usize) -> Vec<Grid> {
         for y in 0..(grid.len() / size) {
             for x in 0..(grid.len() / size) {
                 small_grids.push(vec![
-                    vec![
-                        grid[y * size][x * size],
-                        grid[y * size][x * size + 1],
-                        grid[y * size][x * size + 2],
-                    ],
-                    vec![
-                        grid[y * size + 1][x * size],
-                        grid[y * size + 1][x * size + 1],
-                        grid[y * size + 1][x * size + 2],
-                    ],
-                    vec![
-                        grid[y * size + 2][x * size],
-                        grid[y * size + 2][x * size + 1],
-                        grid[y * size + 2][x * size + 2],
-                    ],
+                    BitVec::from_fn(3, |i| grid[y * size][x * size + i]),
+                    BitVec::from_fn(3, |i| grid[y * size + 1][x * size + i]),
+                    BitVec::from_fn(3, |i| grid[y * size + 2][x * size + i]),
                 ]);
             }
         }
@@ -50,19 +36,22 @@ fn small_grids(grid: &Grid, size: usize) -> Vec<Grid> {
 fn flip(grid: &Grid) -> Grid {
     let mut new_grid = Vec::new();
     for row in grid {
-        new_grid.push(row.iter().copied().rev().collect());
+        new_grid.push(BitVec::from_iter(row.iter().rev()));
     }
     new_grid
 }
 
 fn rotate(grid: &Grid) -> Grid {
     if grid.len() == 2 {
-        vec![vec![grid[0][1], grid[1][1]], vec![grid[0][0], grid[1][0]]]
+        vec![
+            BitVec::from_fn(2, |i| grid[i][1]),
+            BitVec::from_fn(2, |i| grid[i][0]),
+        ]
     } else {
         vec![
-            vec![grid[0][2], grid[1][2], grid[2][2]],
-            vec![grid[0][1], grid[1][1], grid[2][1]],
-            vec![grid[0][0], grid[1][0], grid[2][0]],
+            BitVec::from_fn(3, |i| grid[i][2]),
+            BitVec::from_fn(3, |i| grid[i][1]),
+            BitVec::from_fn(3, |i| grid[i][0]),
         ]
     }
 }
@@ -107,9 +96,9 @@ pub fn main() -> Result<(usize, usize)> {
     let mut solution_a = 0;
 
     let mut grid: Grid = vec![
-        vec![false, true, false],
-        vec![false, false, true],
-        vec![true, true, true],
+        BitVec::from_fn(3, |i| i == 1),
+        BitVec::from_fn(3, |i| i == 2),
+        BitVec::from_elem(3, true),
     ];
 
     for iteration in 0..18 {
@@ -117,9 +106,12 @@ pub fn main() -> Result<(usize, usize)> {
         if current_size % 2 == 0 {
             let small_grids = small_grids(&grid, 2);
             for row in &mut grid {
-                row.resize((current_size / 2) * 3, false);
+                row.extend(repeat_n(false, (current_size / 2) * 3 - current_size));
             }
-            grid.resize((current_size / 2) * 3, vec![false; (current_size / 2) * 3]);
+            grid.resize(
+                (current_size / 2) * 3,
+                BitVec::from_elem((current_size / 2) * 3, false),
+            );
 
             let mut y = 0;
             let mut x = 0;
@@ -136,17 +128,18 @@ pub fn main() -> Result<(usize, usize)> {
                 for _ in 0..2 {
                     for _ in 0..4 {
                         if let Some(output) = two_rules.get(&current) {
-                            grid[y * 3][x * 3] = output[0][0];
-                            grid[y * 3][x * 3 + 1] = output[0][1];
-                            grid[y * 3][x * 3 + 2] = output[0][2];
+                            grid[y * 3].set(x * 3, output[0][0]);
+                            grid[y * 3].set(x * 3 + 1, output[0][1]);
+                            grid[y * 3].set(x * 3 + 2, output[0][2]);
 
-                            grid[y * 3 + 1][x * 3] = output[1][0];
-                            grid[y * 3 + 1][x * 3 + 1] = output[1][1];
-                            grid[y * 3 + 1][x * 3 + 2] = output[1][2];
+                            grid[y * 3 + 1].set(x * 3, output[1][0]);
+                            grid[y * 3 + 1].set(x * 3 + 1, output[1][1]);
+                            grid[y * 3 + 1].set(x * 3 + 2, output[1][2]);
 
-                            grid[y * 3 + 2][x * 3] = output[2][0];
-                            grid[y * 3 + 2][x * 3 + 1] = output[2][1];
-                            grid[y * 3 + 2][x * 3 + 2] = output[2][2];
+                            grid[y * 3 + 2].set(x * 3, output[2][0]);
+                            grid[y * 3 + 2].set(x * 3 + 1, output[2][1]);
+                            grid[y * 3 + 2].set(x * 3 + 2, output[2][2]);
+
                             continue 'grid_loop;
                         }
                         current = rotate(&current);
@@ -158,9 +151,12 @@ pub fn main() -> Result<(usize, usize)> {
         } else {
             let small_grids = small_grids(&grid, 3);
             for row in &mut grid {
-                row.resize((current_size / 3) * 4, false);
+                row.extend(repeat_n(false, (current_size / 3) * 4 - current_size));
             }
-            grid.resize((current_size / 3) * 4, vec![false; (current_size / 3) * 4]);
+            grid.resize(
+                (current_size / 3) * 4,
+                BitVec::from_elem((current_size / 3) * 4, false),
+            );
 
             let mut y = 0;
             let mut x = 0;
@@ -177,25 +173,25 @@ pub fn main() -> Result<(usize, usize)> {
                 for _ in 0..2 {
                     for _ in 0..4 {
                         if let Some(output) = three_rules.get(&current) {
-                            grid[y * 4][x * 4] = output[0][0];
-                            grid[y * 4][x * 4 + 1] = output[0][1];
-                            grid[y * 4][x * 4 + 2] = output[0][2];
-                            grid[y * 4][x * 4 + 3] = output[0][3];
+                            grid[y * 4].set(x * 4, output[0][0]);
+                            grid[y * 4].set(x * 4 + 1, output[0][1]);
+                            grid[y * 4].set(x * 4 + 2, output[0][2]);
+                            grid[y * 4].set(x * 4 + 3, output[0][3]);
 
-                            grid[y * 4 + 1][x * 4] = output[1][0];
-                            grid[y * 4 + 1][x * 4 + 1] = output[1][1];
-                            grid[y * 4 + 1][x * 4 + 2] = output[1][2];
-                            grid[y * 4 + 1][x * 4 + 3] = output[1][3];
+                            grid[y * 4 + 1].set(x * 4, output[1][0]);
+                            grid[y * 4 + 1].set(x * 4 + 1, output[1][1]);
+                            grid[y * 4 + 1].set(x * 4 + 2, output[1][2]);
+                            grid[y * 4 + 1].set(x * 4 + 3, output[1][3]);
 
-                            grid[y * 4 + 2][x * 4] = output[2][0];
-                            grid[y * 4 + 2][x * 4 + 1] = output[2][1];
-                            grid[y * 4 + 2][x * 4 + 2] = output[2][2];
-                            grid[y * 4 + 2][x * 4 + 3] = output[2][3];
+                            grid[y * 4 + 2].set(x * 4, output[2][0]);
+                            grid[y * 4 + 2].set(x * 4 + 1, output[2][1]);
+                            grid[y * 4 + 2].set(x * 4 + 2, output[2][2]);
+                            grid[y * 4 + 2].set(x * 4 + 3, output[2][3]);
 
-                            grid[y * 4 + 3][x * 4] = output[3][0];
-                            grid[y * 4 + 3][x * 4 + 1] = output[3][1];
-                            grid[y * 4 + 3][x * 4 + 2] = output[3][2];
-                            grid[y * 4 + 3][x * 4 + 3] = output[3][3];
+                            grid[y * 4 + 3].set(x * 4, output[3][0]);
+                            grid[y * 4 + 3].set(x * 4 + 1, output[3][1]);
+                            grid[y * 4 + 3].set(x * 4 + 2, output[3][2]);
+                            grid[y * 4 + 3].set(x * 4 + 3, output[3][3]);
                             continue 'grid_loop;
                         }
                         current = rotate(&current);
@@ -206,7 +202,7 @@ pub fn main() -> Result<(usize, usize)> {
             }
         }
         if iteration == 4 {
-            solution_a = grid.iter().map(|r| r.iter().filter(|b| **b).count()).sum();
+            solution_a = grid.iter().map(|r| r.iter().filter(|b| *b).count()).sum();
         }
     }
 
--- src/day24.rs
+++ src/day24.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 pub fn main() -> Result<(i64, i64)> {
     let lines = common::read_lines("inputs/24.txt")?;
 
-    let mut components: Vec<(i64, i64)> = Vec::new();
+    let mut components: Vec<(i64, i64, bool)> = Vec::new();
 
     for line in lines {
         let line = line?;
@@ -14,38 +14,31 @@ pub fn main() -> Result<(i64, i64)> {
         let (from, to) = line
             .split_once('/')
             .expect("Expected a valid bridge component");
-        components.push((from.parse::<i64>()?, to.parse::<i64>()?));
+        components.push((from.parse::<i64>()?, to.parse::<i64>()?, false));
     }
 
-    components.sort_unstable_by_key(|(from, to)| (*from).min(*to));
+    components.sort_unstable_by_key(|(from, to, _)| (*from).min(*to));
 
-    let solution_a = strongest_bridge(&components, &mut Vec::new(), 0);
-    let solution_b = longest_bridge(&components, &mut Vec::new(), 0).0;
+    let solution_a = strongest_bridge(&mut components, 0);
+    let solution_b = longest_bridge(&mut components, 0).0;
 
     Ok((solution_a, solution_b))
 }
 
-fn strongest_bridge(
-    components: &Vec<(i64, i64)>,
-    used: &mut Vec<(i64, i64)>,
-    required: i64,
-) -> i64 {
+fn strongest_bridge(components: &mut Vec<(i64, i64, bool)>, required: i64) -> i64 {
     let mut max = 0;
-    let i = components.partition_point(|&(l, r)| l.min(r) <= required);
-    for component in components[..i].iter() {
-        if used.contains(component) {
-            continue;
-        }
-        match component {
-            (l, r) if *l == required => {
-                used.push(*component);
-                max = max.max(required + *r + strongest_bridge(components, used, *r));
-                used.pop();
+    for j in 0..components.partition_point(|&(l, r, _)| l.min(r) <= required) {
+        match components[j] {
+            (_, _, true) => continue,
+            (l, r, _) if l == required => {
+                components[j].2 = true;
+                max = max.max(required + r + strongest_bridge(components, r));
+                components[j].2 = false;
             }
-            (l, r) if *r == required => {
-                used.push(*component);
-                max = max.max(required + *l + strongest_bridge(components, used, *l));
-                used.pop();
+            (l, r, _) if r == required => {
+                components[j].2 = true;
+                max = max.max(required + l + strongest_bridge(components, l));
+                components[j].2 = false;
             }
             _ => {}
         }
@@ -53,22 +46,15 @@ fn strongest_bridge(
     max
 }
 
-fn longest_bridge(
-    components: &Vec<(i64, i64)>,
-    used: &mut Vec<(i64, i64)>,
-    required: i64,
-) -> (i64, usize) {
+fn longest_bridge(components: &mut Vec<(i64, i64, bool)>, required: i64) -> (i64, usize) {
     let mut max = (0, 0);
-    let i = components.partition_point(|&(l, r)| l.min(r) <= required);
-    for component in components[..i].iter() {
-        if used.contains(component) {
-            continue;
-        }
-        match component {
-            (l, r) if *l == required => {
-                used.push(*component);
-                let (strength, length) = longest_bridge(components, used, *r);
-                let strength = *l + *r + strength;
+    for j in 0..components.partition_point(|&(l, r, _)| l.min(r) <= required) {
+        match components[j] {
+            (_, _, true) => continue,
+            (l, r, _) if l == required => {
+                components[j].2 = true;
+                let (strength, length) = longest_bridge(components, r);
+                let strength = l + r + strength;
                 let length = length + 1;
                 match length.cmp(&max.1) {
                     Ordering::Less => {}
@@ -77,12 +63,12 @@ fn longest_bridge(
                     }
                     Ordering::Greater => max = (strength, length),
                 }
-                used.pop();
+                components[j].2 = false;
             }
-            (l, r) if *r == required => {
-                used.push(*component);
-                let (strength, length) = longest_bridge(components, used, *l);
-                let strength = *l + *r + strength;
+            (l, r, _) if r == required => {
+                components[j].2 = true;
+                let (strength, length) = longest_bridge(components, l);
+                let strength = l + r + strength;
                 let length = length + 1;
                 match length.cmp(&max.1) {
                     Ordering::Less => {}
@@ -91,7 +77,7 @@ fn longest_bridge(
                     }
                     Ordering::Greater => max = (strength, length),
                 }
-                used.pop();
+                components[j].2 = false;
             }
             _ => {}
         }
